c74be790e896d211118891a0d2b4e7370ddbd44a
emreacar <acaremre@me.com>
2020-05-12 02:40:32 +0300

    Server Logic Changes

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ import Radius from './lib/node-radius'
 const debugMsg = require('debug')('debug')
 const server = new Radius()
 
-server.on('accounting', function(req, res) {
-  debugMsg(req.data.AttributeList)
-})
-
-server.on('authorization', function(req, res) {
+server.on('request', function(req, res) {
+  debugMsg(req.data)
   debugMsg(req.data.AttributeList)
 })
 
