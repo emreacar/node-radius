d9ab4676719b5b85a064cdc00f570d5552a51af9
emreacar <acaremre@me.com>
2020-05-12 05:22:21 +0300

    Middleware Management Added

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import Radius from './lib/node-radius'
 const debugMsg = require('debug')('debug')
 const server = new Radius()
 
-server.on('request', function(req, res) {
-  debugMsg(req.data)
-  debugMsg(req.data.AttributeList)
+server.on('request', function(req, res, next) {
+  console.log(req)
 })
 
 server.start()
