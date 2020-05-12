576bd5586598c0af50c0f1086a75a3af5f258a17
emreacar <acaremre@me.com>
2020-05-12 06:15:51 +0300

    Todo and Clients

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import Radius from './lib/node-radius'
 
-const debugMsg = require('debug')('debug')
 const server = new Radius()
+
+const clients = [
+  { ip: '127.0.0.1', secret: 'secret', name: 'NAS 1' },
+  { ip: '127.0.0.1', secret: 'secret', name: 'NAS 2' }
+]
+
+server.addClient(clients)
 
 server.use((req, res, next) => {
   console.log('NABER LO?')
@@ -10,6 +16,10 @@ server.use((req, res, next) => {
 
 server.on('request', function(req, res, next) {
   console.log(req)
+})
+
+server.on('error', function(error) {
+  console.log(error)
 })
 
 server.start()
