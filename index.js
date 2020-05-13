78665c95957813dc81a2f83e882b641593327e4c
emreacar <acaremre@me.com>
2020-05-14 01:26:42 +0300

    Packade Class

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ import Radius from './lib/node-radius'
 const server = new Radius()
 
 const clients = [
-  { ip: '127.0.0.1', secret: 'secret', name: 'NAS 1' },
-  { ip: '127.0.0.1', secret: 'secret', name: 'NAS 2' }
+  { ip: '127.0.0.1', secret: 'secret', name: 'NAS-1' },
+  { ip: '192.168.1.2', secret: 'secret', name: 'NAS-2' }
 ]
 
 server.addClient(clients)
 
 server.use((req, res, next) => {
-  console.log('NABER LO?')
-  next();
+  console.log(req.attr.UserName)
+  console.log(req.attr.UserPassword2)
+
+  next()
 })
 
-server.on('request', function(req, res, next) {
-  console.log(req)
+
+
+server.on('request', function(req, res) {
+  res.end()
 })
 
 server.on('error', function(error) {
