9e0cecff668fef91143a9ad4875a5bc0c9c55971
emreacar <acaremre@me.com>
2021-06-23 14:08:24 +0300

    Added Hostname Option

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,9 @@ require("dgram");
 const helpers_1 = require("./helpers");
 require("./types");
 exports.RLogger = (level, message) => {
-    if (helpers_1.ConfigMan.get('logLevels').console && helpers_1.ConfigMan.get('logLevels').console === 1) {
+    if (helpers_1.ConfigMan.get('logLevels').console &&
+        helpers_1.ConfigMan.get('logLevels').console === 1 &&
+        level !== 'debug') {
         console.log(level, message);
     }
     if (Object.keys(helpers_1.ConfigMan.get('logLevels')).includes(level) &&
