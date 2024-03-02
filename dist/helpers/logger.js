4b0b74bb40cd2d46237cbf8598b62421d620da0a
Emre Acar <acaremre@me.com>
2024-03-02 22:49:28 +0300

    feat(build)

--- dist/helpers/logger.js
+++ dist/helpers/logger.js
@@ -1,26 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Logger = void 0;
-const graylog2_1 = __importDefault(require("graylog2"));
-const os_1 = require("os");
-const gLogger = new graylog2_1.default.graylog({
-    servers: [
-        {
-            host: 'nglog.turancoskun.com',
-            port: 49514
-        }
-    ],
-    hostname: os_1.hostname() || 'node-radius',
-    facility: 'Radius'
-});
-gLogger.on('error', function (error) {
-    console.error(`(PID: ${process.pid}) Error while trying to write to graylog2:`, error);
-});
 exports.Logger = params => {
-    gLogger.log(params);
+    console.log('LOG', params);
 };
 exports.default = exports.Logger;
 //# sourceMappingURL=logger.js.map
--- helpers/logger.ts
+++ helpers/logger.ts
@@ -1,23 +1,23 @@
-import graylog2 from 'graylog2'
-import { hostname } from 'os'
+// import graylog2 from 'graylog2'
+// import { hostname } from 'os'
 
-const gLogger = new graylog2.graylog({
-  servers: [
-    {
-      host: '',
-      port: 49514
-    }
-  ],
-  hostname: hostname() || 'node-radius',
-  facility: 'Radius'
-})
+// const gLogger = new graylog2.graylog({
+//   servers: [
+//     {
+//       host: '',
+//       port: 49514
+//     }
+//   ],
+//   hostname: hostname() || 'node-radius',
+//   facility: 'Radius'
+// })
 
-gLogger.on('error', function (error) {
-  console.error(`(PID: ${process.pid}) Error while trying to write to graylog2:`, error)
-})
+// gLogger.on('error', function (error) {
+//   console.error(`(PID: ${process.pid}) Error while trying to write to graylog2:`, error)
+// })
 
 export const Logger = params => {
-  console.log('LOG', params);
+  console.log('LOG', params)
   // gLogger.log(params)
 }
 
